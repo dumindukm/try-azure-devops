9b6351cacf890b33e5de16180a785ca3eaf56b1b
Dumindu <dumindukm@gmail.com>
2021-07-09 12:59:56 +0530

    added xUnit project

---
+++ code/web-app/ToDoApp/ToDoApp/Features/ToDoService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Features
+{
+    public class ToDoService
+    {
+        public List<ToDoItem> GetToDoItems()
+        {
+            List<ToDoItem> items = new List<ToDoItem>();
+
+            return items;
+        }
+    }
+
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
